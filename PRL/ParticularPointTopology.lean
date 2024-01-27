5bbf6517f91fa41aa9e4374fe3244d63f542e346
Luigi Massacci <luigi.massacci.it@gmail.com>
2024-01-27 21:27:50 +0100

    Neat Particular Point Topology

--- PRL/ParticularPointTopology.lean
+++ PRL/ParticularPointTopology.lean
@@ -4,40 +4,29 @@ import Mathlib.Topology.Order
 
 open Set Filter Topology TopologicalSpace
 
-@[simp]
-def ParticularPointTopology {α : Type*} (p : α) := {∅} ∪ {S : Set α | p ∈ S}
+def ParticularPointTopology (α : Type*) (p : α) := α
+
+namespace ParticularPointTopology
+
+instance {α : Type*} (p : α) : Inhabited (ParticularPointTopology α p) where
+  default := p
+
+def of : α ≃ ParticularPointTopology α p:=
+  Equiv.refl α
 
 variable {α : Type*} (p : α)
 
-instance : TopologicalSpace α where
-  IsOpen := ParticularPointTopology p
-  isOpen_univ := by
-    simp
-    right
-    trivial
-  isOpen_inter := by
-    simp; refine fun s t hs ht ↦ ?_
-    rcases hs with e | ne
-    left
-    aesop
-    rcases ht with e | ne₁
-    left
-    aesop
-    right
-    exact ⟨ne, ne₁⟩
+instance : TopologicalSpace (ParticularPointTopology α p) where
+  IsOpen s := s.Nonempty → p ∈ s
+  isOpen_univ := by simp
+  isOpen_inter s t := by
+    rintro hs ht ⟨x, hxs, hxt⟩
+    exact ⟨hs ⟨x, hxs⟩, ht ⟨x, hxt⟩⟩
   isOpen_sUnion := by
-    simp; intro S hS
-    by_cases h: Set.Nonempty (⋃₀ S)
-    choose! w hw using (Nonempty.of_sUnion h)
-    rcases (hS w hw) with e | ne
-    left
-    sorry
-    sorry
-    sorry
+    rintro s h ⟨x, t, hts, hxt⟩
+    exact mem_sUnion_of_mem (h t hts ⟨x, hxt⟩) hts
 
 
-
-namespace ParticularPointTopology
 
 
 
