2d94ce53a46cb12ac172d9af3f2396262c9e610f
Luigi Massacci <luigi.massacci.it@gmail.com>
2024-02-20 22:23:27 +0100

    Progress on katetov

--- PRL/Katetov.lean
+++ PRL/Katetov.lean
@@ -1,7 +1,7 @@
 import Mathlib.Tactic
 import Mathlib.Topology.Compactification.OnePoint
 
-variable {X : Type _} [MetricSpace X]
+variable {X : Type _} [MetricSpace X] [Nonempty X]
 
 class isKatetov (f : X → ℝ) : Prop where
   le_dist : ∀ x y, |f x - f y| ≤ dist x y
@@ -33,3 +33,49 @@ instance (f : X → ℝ) (h : isKatetov f) : PseudoMetricSpace (OnePoint X) wher
       linarith
     · exact dist_triangle x y z
   edist_dist x y := by exact ENNReal.coe_nnreal_eq _
+
+
+def E (X: Type*) [MetricSpace X] := {f : X → ℝ | isKatetov f}
+
+theorem E_def : f ∈ E X ↔ isKatetov f := by simp [E]
+
+instance : MetricSpace (E X) where
+  dist f g := sSup {|(f : X → ℝ) x - (g : X → ℝ) x| | x : X}
+  dist_self f := by simp [dist]
+  dist_comm f g := by simp [dist, abs_sub_comm]
+  dist_triangle f g h := by
+    have x₀ := Classical.choice ‹Nonempty X›
+    have hf : ∀ x, |(f : X → ℝ) x - dist x x₀| ≤ (f : X → ℝ) x₀ := by
+      intro x
+      have := @isKatetov.le_add _ _ f (E_def.mp (by aesop)) x x₀
+      refine abs_le.mpr ?_
+      constructor
+      · linarith
+      · have := le_of_abs_le (@isKatetov.le_dist _ _ f (E_def.mp (by aesop)) x x₀)
+        linarith
+    have hg : ∀ x, |(g : X → ℝ) x - dist x x₀| ≤ (g : X → ℝ) x₀ := by
+      intro x
+      have := @isKatetov.le_add _ _ g (E_def.mp (by aesop)) x x₀
+      refine abs_le.mpr ?_
+      constructor
+      · linarith
+      · have := le_of_abs_le (@isKatetov.le_dist _ _ g (E_def.mp (by aesop)) x x₀)
+        linarith
+    have h₁: ∀ x, |(f : X → ℝ) x - dist x x₀| + |(g : X → ℝ) x - dist x x₀|  ≤ (f : X → ℝ) x₀ + (g : X → ℝ) x₀ := by
+      intro x
+      exact add_le_add (hf x) (hg x)
+    have h₂ : ∀ (x : X), |(f : X → ℝ)  x - (g : X → ℝ) x| ≤ |(f : X → ℝ) x - dist x x₀| + |(g : X → ℝ) x - dist x x₀|:= by
+      intro x
+      rw [← abs_sub_comm (dist x x₀) ((g : X → ℝ) x)]
+      apply abs_sub_le ((f : X → ℝ) x) (dist x x₀) ((g : X → ℝ) x)
+    have : ∀ x, |(f : X → ℝ) x - (g : X → ℝ) x| ≤ (f : X → ℝ) x₀ + (g : X → ℝ) x₀ := by
+      intro x
+      exact le_trans (h₂ x) (h₁ x)
+    simp [dist]
+    sorry
+    -- set s := fun (f g : E X) ↦ {|(f : X → ℝ) x - (g : X → ℝ) x| | x : X} with s_def
+    -- have : ∀ f g, Set.Nonempty (s f g) := by
+    --   intro f g
+    --   simp [s_def]
+    --   use |(f : X → ℝ) x₀ - (g : X → ℝ) x₀|
+    --   simp
