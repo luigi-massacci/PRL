8306600eece57503584367e5e715ce0d0099dbe6
Luigi Massacci <luigi.massacci.it@gmail.com>
2024-02-22 11:49:28 +0100

    E(X) is a metric spacegit add -A

--- PRL/Katetov.lean
+++ PRL/Katetov.lean
@@ -1,7 +1,9 @@
 import Mathlib.Tactic
 import Mathlib.Topology.Compactification.OnePoint
+import Mathlib.Data.Real.Archimedean
+import Mathlib.Algebra.Order.Pointwise
 
-variable {X : Type _} [MetricSpace X] [Nonempty X]
+variable {X : Type _} [MetricSpace X]
 
 class isKatetov (f : X → ℝ) : Prop where
   le_dist : ∀ x y, |f x - f y| ≤ dist x y
@@ -11,7 +13,7 @@ theorem katetov_nonneg {f : X → ℝ} (h : isKatetov f) (x : X) : 0 ≤ f x := 
   have : 0 ≤ f x + f x := by rw [← dist_self x]; exact isKatetov.le_add x x
   linarith
 
-instance (f : X → ℝ) (h : isKatetov f) : PseudoMetricSpace (OnePoint X) where
+instance {f : X → ℝ} (h : isKatetov f) : PseudoMetricSpace (OnePoint X) where
   dist x y :=
     match x, y with
     | none, none => 0
@@ -39,43 +41,64 @@ def E (X: Type*) [MetricSpace X] := {f : X → ℝ | isKatetov f}
 
 theorem E_def : f ∈ E X ↔ isKatetov f := by simp [E]
 
-instance : MetricSpace (E X) where
+lemma helper (x₀ : X) (h : isKatetov f) : ∀ x, |f x - dist x x₀| ≤ f x₀ := by
+  refine fun x ↦ abs_le.mpr ?_
+  constructor
+  · have := @isKatetov.le_add _ _ f h x x₀
+    linarith
+  · have := le_of_abs_le (@isKatetov.le_dist _ _ f h x x₀)
+    linarith
+
+theorem bounded_dist {f g : X → ℝ} (hf : isKatetov f) (hg : isKatetov g) : BddAbove {|f x - g x| | x : X} := by
+  by_cases hn : Nonempty X
+  · have x₀ := Classical.choice ‹Nonempty X›
+    refine ⟨f x₀ + g x₀, fun _ ⟨x, hx⟩ ↦ ?_⟩
+    rw [← hx]
+    have h₂ : |f x - g x| ≤ |f x - dist x x₀| + |g x - dist x x₀|:= by
+      rw [← abs_sub_comm (dist x x₀) (g x)]
+      apply abs_sub_le (f x) (dist x x₀) (g x)
+    exact le_trans h₂ (add_le_add (helper x₀ hf x) (helper x₀ hg x))
+  · refine ⟨0, fun _ ⟨x, hx⟩ ↦ False.elim (hn ⟨x⟩)⟩
+
+lemma sSup_eq_zero (s : Set ℝ) (hb : BddAbove s) (snonneg : ∀ x ∈ s, 0 ≤ x) (hsup : sSup s = 0) : ∀ x ∈ s, x = 0 := by
+  intro x xs
+  apply le_antisymm
+  rw [← hsup]
+  exact le_csSup hb xs
+  exact snonneg x xs
+
+noncomputable instance [Nonempty X] : MetricSpace (E X) where
   dist f g := sSup {|(f : X → ℝ) x - (g : X → ℝ) x| | x : X}
   dist_self f := by simp [dist]
   dist_comm f g := by simp [dist, abs_sub_comm]
   dist_triangle f g h := by
-    have x₀ := Classical.choice ‹Nonempty X›
-    have hf : ∀ x, |(f : X → ℝ) x - dist x x₀| ≤ (f : X → ℝ) x₀ := by
-      intro x
-      have := @isKatetov.le_add _ _ f (E_def.mp (by aesop)) x x₀
-      refine abs_le.mpr ?_
-      constructor
-      · linarith
-      · have := le_of_abs_le (@isKatetov.le_dist _ _ f (E_def.mp (by aesop)) x x₀)
-        linarith
-    have hg : ∀ x, |(g : X → ℝ) x - dist x x₀| ≤ (g : X → ℝ) x₀ := by
-      intro x
-      have := @isKatetov.le_add _ _ g (E_def.mp (by aesop)) x x₀
-      refine abs_le.mpr ?_
-      constructor
-      · linarith
-      · have := le_of_abs_le (@isKatetov.le_dist _ _ g (E_def.mp (by aesop)) x x₀)
-        linarith
-    have h₁: ∀ x, |(f : X → ℝ) x - dist x x₀| + |(g : X → ℝ) x - dist x x₀|  ≤ (f : X → ℝ) x₀ + (g : X → ℝ) x₀ := by
-      intro x
-      exact add_le_add (hf x) (hg x)
-    have h₂ : ∀ (x : X), |(f : X → ℝ)  x - (g : X → ℝ) x| ≤ |(f : X → ℝ) x - dist x x₀| + |(g : X → ℝ) x - dist x x₀|:= by
-      intro x
-      rw [← abs_sub_comm (dist x x₀) ((g : X → ℝ) x)]
-      apply abs_sub_le ((f : X → ℝ) x) (dist x x₀) ((g : X → ℝ) x)
-    have : ∀ x, |(f : X → ℝ) x - (g : X → ℝ) x| ≤ (f : X → ℝ) x₀ + (g : X → ℝ) x₀ := by
-      intro x
-      exact le_trans (h₂ x) (h₁ x)
     simp [dist]
-    sorry
-    -- set s := fun (f g : E X) ↦ {|(f : X → ℝ) x - (g : X → ℝ) x| | x : X} with s_def
-    -- have : ∀ f g, Set.Nonempty (s f g) := by
-    --   intro f g
-    --   simp [s_def]
-    --   use |(f : X → ℝ) x₀ - (g : X → ℝ) x₀|
-    --   simp
+    apply Real.sSup_le
+    · rintro val ⟨x, rfl⟩
+      rw [← csSup_add]
+      · apply le_trans <| abs_sub_le ((f : X → ℝ) x) ((g : X → ℝ) x) ((h : X → ℝ) x)
+        apply le_csSup
+        · apply BddAbove.add <;> apply bounded_dist <;> aesop
+        · refine Set.mem_add.mpr ⟨|(f : X → ℝ) x - (g : X → ℝ) x|, |(g : X → ℝ) x - (h : X → ℝ) x|, ?_⟩
+          simp
+      · have x₀ := Classical.choice ‹Nonempty X›
+        use |(f : X → ℝ) x₀ - (g : X → ℝ) x₀| ; simp
+      · apply bounded_dist <;> aesop
+      · have x₀ := Classical.choice ‹Nonempty X›
+        use |(g : X → ℝ) x₀ - (h : X → ℝ) x₀| ; simp
+      · apply bounded_dist <;> aesop
+    · apply add_nonneg <;>
+      {
+        apply Real.sSup_nonneg
+        rintro val ⟨x, rfl⟩
+        apply abs_nonneg
+      }
+  eq_of_dist_eq_zero := by
+    intro f g h
+    simp [dist] at h
+    apply sSup_eq_zero at h
+    · ext x
+      exact eq_of_sub_eq_zero <| abs_eq_zero.mp (h |(f : X → ℝ) x - (g : X → ℝ) x| ⟨x, rfl⟩)
+    · apply bounded_dist <;> aesop
+    · rintro _ ⟨x, rfl⟩; exact abs_nonneg _
+  edist_dist x y:= by exact ENNReal.coe_nnreal_eq _
